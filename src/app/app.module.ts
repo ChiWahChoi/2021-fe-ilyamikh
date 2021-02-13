cf14ed0e3c42c662bd8d18ebf0d9a3502e2ebb1f
ilya <ilya.mikh@outlook.com>
2021-02-13 20:18:03 +0100

    deleted tested components
    add models les and priority

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { RecipeModule } from './recipe/recipe.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule,
-    RecipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/Les.ts
@@ -0,0 +1,10 @@
+export class Les{
+  id: number;
+  lesTitle: string;
+
+  constructor(id: number, lesTitle: string) {
+    this.id = id;
+    this.lesTitle = lesTitle;
+  }
+}
+
