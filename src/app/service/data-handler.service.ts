078ec062e72260a889e0125c5c08d4ca5f5be73f
ilya <ilya.mikhaylov@student.hogent.be>
2021-03-03 21:15:59 +0100

    add some empty methods to connect api

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Theme } from '../models/Theme';
 import {TestData} from '../testdata/TestData';
 import {Chapter} from '../models/Chapter';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
+import {Priority} from '../models/Priority';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,83 @@ export class DataHandlerService {
   public chaptersSubject$ = new BehaviorSubject<Chapter[]>(TestData.chapters);
   public themeSubject$ = new BehaviorSubject<Theme[]>(TestData.themes);
 
+
+
+
+
   constructor() {
 
   }
-  /*
-  getThemes(): Theme[]{
-    return TestData.themes;
-    is nu themeSubject behaviourSubject geworden
+
+
+  getChapter$(chapterId: number) : Observable<Chapter>{
+    return null;
   }
 
-  fillChapters() {
-    this.chaptersSubject.next(TestData.chapters);
+  getChaptersByPriorityOrTheme$(priorityId?: number, themeId?: number) : Observable<Chapter[]>{
+    return null;
   }
-*/
+
+  updateChapter$(chapterId: number) : void {
+
+  }
+
+  deleteChapter$(chapterId: number) : void{
+
+  }
+
+  addChapter$(chapter: Chapter) : void{
+
+  }
+
+  /////////////////////////////////////////////////////
+
+
+  getAllPriorities$() : Observable<Priority[]>{
+    return null;
+  }
+
+  addPriority$(priority: Priority){
+
+  }
+
+  getPriority$() : Observable<Priority>{
+    return null;
+  }
+
+  deletePriority$(priorityId: number){
+
+  }
+
+  updatePriority$(priorityId: number){
+
+  }
+
+  ////////////////////////////////////////////////////////
+
+
+  getAllThemes$() : Observable<Theme[]>{
+    return null;
+  }
+
+  addTheme$(theme: Theme){
+
+  }
+
+  getTheme$() : Observable<Theme>{
+    return null;
+  }
+
+  deleteTheme$(themeId: number){
+
+  }
+
+  updateTheme$(themeId: number){
+
+  }
+
+  /////////////////////////////////////////////////////////////
+
 
   fillChaptersByTheme(theme: Theme){
     this.chaptersSubject$.next(TestData.chapters.filter(element => element.theme === theme));
